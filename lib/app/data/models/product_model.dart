0fb5542586e3b91105781b3e7471bdfb764522c8
devtvas <tarcisio.word@gmail.com>
2024-08-10 23:27:09 -0500

    feat - update model

--- lib/app/data/models/product_model.dart
+++ lib/app/data/models/product_model.dart
@@ -1,17 +1,47 @@
 class ProductModel {
+  final String id;
   final String name;
+  final String description;
+  final String category;
+  final String image;
+  final String price;
+  final String material;
+  final String department;
 
-  ProductModel({required this.name});
+  ProductModel({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.category,
+    required this.image,
+    required this.price,
+    required this.material,
+    required this.department,
+  });
 
-  static fromJson(json) {
+  factory ProductModel.fromJson(Map<String, dynamic> json) {
     return ProductModel(
-      name: json['name'],
+      id: json['id'] as String,
+      name: json['name'] as String,
+      description: json['description'] as String,
+      category: json['category'] as String,
+      image: json['image'] as String,
+      price: json['price'] as String,
+      material: json['material'] as String,
+      department: json['department'] as String,
     );
   }
 
   Map<String, dynamic> toJson() {
     return {
+      'id': id,
       'name': name,
+      'description': description,
+      'category': category,
+      'image': image,
+      'price': price,
+      'material': material,
+      'department': department,
     };
   }
 }
